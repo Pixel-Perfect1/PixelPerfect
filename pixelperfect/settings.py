5736ed0e2bd138b0c23c28f5ee51caa9a65eccf2
Sam Hay <snh68@yahoo.com>
2023-01-05 15:51:16 -0800

    add

--- pixelperfect/settings.py
+++ pixelperfect/settings.py
@@ -131,3 +131,6 @@ LOGOUT_REDIRECT_URL = '/accounts/logout_index'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_on_heroku
+django_on_heroku.settings(locals())
