cec48bc5175c86a0be30cabef8bada6b7cbe9782
Sam Hay <snh68@yahoo.com>
2023-06-30 13:07:20 -0700

    hide secret

--- pixelperfect/settings.py
+++ pixelperfect/settings.py
@@ -13,9 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import environ
+from dotenv import load_dotenv
 environ.Env()
 environ.Env.read_env()
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--a#ne5_*bkbmp2)bzri$tz(95p^)zo5$dat#rsb-)h9*se*s23'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
